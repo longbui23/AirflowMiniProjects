fa3bc506b412770a34981160166ec8322e1c6ae3
Long Bui <112489957+longbui23@users.noreply.github.com>
2024-10-22 01:25:40 -0400

    Add files via upload

---
+++ bank_etl/banks_project.py
@@ -0,0 +1,116 @@
+#packages
+import requests
+from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
+import sqlite3
+from datetime import datetime 
+
+#functions
+def log_progress(message):
+    ''''report log entry for each stages'''
+
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = datetime.now() # get current timestamp 
+    timestamp = now.strftime(timestamp_format) 
+    with open("./code_log.txt","a") as f: 
+        f.write(timestamp + ' : ' + message + '\n')
+
+
+def extract(url, table_attribs):
+    '''retract data by market capitalization'''
+
+    page = requests.get(url).text
+    data = BeautifulSoup(page, "html.parser")
+    df = pd.DataFrame(columns=table_attribs)
+    tables = data.find_all('tbody')
+    rows = tables[0].find_all('tr')
+    
+    for row in rows:
+        col = row.find_all('td')
+        if len(col)!=0:
+            data_dict = {"Name": col[1].find_all("a")[1]["title"],
+                         "MC_USD_Billion": float(col[2].contents[0][:-1])}
+            df1 = pd.DataFrame(data_dict, index=[0])
+            df = pd.concat([df,df1], ignore_index=True)
+    return df
+
+def transform(df, csv_path):
+    '''convert currency'''
+
+    exchange_rate = pd.read_csv(csv_path)
+    exchange_rate = exchange_rate.set_index("Currency").to_dict()["Rate"]
+
+    df["MC_GBP_Billion"] = [np.round(x * exchange_rate["GBP"], 2) for x in df["MC_USD_Billion"]]
+    df["MC_EUR_Billion"] = [np.round(x * exchange_rate["EUR"], 2) for x in df["MC_USD_Billion"]]
+    df["MC_INR_Billion"] = [np.round(x * exchange_rate["INR"], 2) for x in df["MC_USD_Billion"]]
+
+    return df
+
+def load_to_csv(df, output_path):
+    ''' This function saves the final data frame as a CSV file in
+    the provided path. Function returns nothing.'''
+
+    df.to_csv(output_path)
+
+def load_to_db(df, sql_connection, table_name):
+    ''' This function saves the final data frame to a database
+    table with the provided name. Function returns nothing.'''
+
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
+
+def run_query(query_statement, sql_connection):
+    ''' This function runs the query on the database table and
+    prints the output on the terminal. Function returns nothing. '''
+
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
+
+
+
+# Implementation
+url = "https://web.archive.org/web/20230908091635/https://en.wikipedia.org/wiki/List_of_largest_banks"
+csv_path = "./exchange_rate.csv"
+table_attribs = ["Name", "MC_USD_Billion"]
+output_path = "./Largest_banks_data.csv"
+db_name = "Banks.db"
+table_name = "Largest_banks"
+log_file = "./code_log.txt"
+
+log_progress('Preliminaries complete. Initiating ETL process')
+
+df = extract(url, table_attribs)
+log_progress("Data extraction complete. Initiating Transformation process")
+
+df = transform(df, csv_path)
+log_progress('Data transformation complete. Initiating loading process')
+
+load_to_csv(df, output_path)
+log_progress("Data saved to CSV file")
+
+sql_connection = sqlite3.connect(db_name)
+log_progress('SQL Connection initiated.')
+
+load_to_db(df, sql_connection, table_name)
+log_progress('Data loaded to Database as table. Running the query')
+
+
+query_statement = f"SELECT * from {table_name}"
+run_query(query_statement, sql_connection)
+
+query_statement = f"SELECT AVG(MC_GBP_Billion) FROM {table_name}"
+run_query(query_statement, sql_connection)
+
+query_statement = f"SELECT Name from {table_name} LIMIT 5"
+run_query(query_statement, sql_connection)
+
+log_progress("Process Complete")
+
+
+sql_connection.close()
+log_progress("Server Connection closed")
+
+with open(log_file, "r") as log:
+    LogContent = log.read()
+    print(LogContent)
